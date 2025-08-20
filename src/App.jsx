29dea8b134be7bc4498b0bfeef40f89ee0721b7c
johannmaxime <johannmaxime@yahoo.fr>
2025-08-20 12:01:33 +0100

    api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Search from "./components/Search.jsx";
 
-const API_BASE_URL = 'https://api.themoviedb.org'
+const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const API_OPTIONS = {
@@ -14,9 +14,39 @@ const API_OPTIONS = {
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage('')
+
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movies?sort_by=popularity.desc`;
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if (!response.ok) {
+                throw new Error('impossible de récupérer les films');
+            }
+            const data = await response.json();
+            
+            if(data.response === 'false'){
+                setErrorMessage(data.Error ||'Aucun film trouvé');
+                setMovieList([]);
+                return;
+            }
+            setMovieList(data.results || []);
+        } catch (error) {
+            console.error(`Error fetching movies: ${error}`);
+            setErrorMessage('Une erreur s\'est produite lors de la récupération des films. Veuillez réessayer plus tard.');
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     useEffect(() => {
-
+        fetchMovies();
     }, []);
 
     return (
@@ -26,9 +56,23 @@ const App = () => {
             <header>
                 <img src="./hero.png" alt="hero banner"/>
                 <h1>Trouver des <span className="text-gradient">films</span> pour vos moments détentes</h1>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             </header>
 
-            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+            <section className="all-movies">
+                <h2>Tout les films</h2>
+
+                {isLoading ? (
+                   <p className="text-white">Chargement...</p>
+                ) : errorMessage ? (
+                    <p className="text-red-500">{errorMessage}</p>
+                ) :(
+                    <ul>
+                        
+                    </ul>
+                )}
+            </section>
+
 
         </main>
     )
